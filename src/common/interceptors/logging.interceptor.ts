9b5cf621fc5c3cdb7e495dc092a3c98784f8acb7
Jatin Garg <jatingarg12356@gmail.com>
2026-07-02 21:34:47 +0530

    fix: resolve logs module import paths with baseUrl config

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -6,9 +6,7 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-
-import { LogsService } from '../../logs/logs.service';
-
+import { LogsService } from 'logs/logs.service';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   constructor(private readonly logsService: LogsService) {}
